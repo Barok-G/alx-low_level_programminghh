dbe108806168f7b8cc19f8eb7b2dffc7ed030532
Biruk <leoseven59@gmail.com>
2022-06-07 14:40:46 +0300

    mopl

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,20 @@
 #include "main.h"
-/*including
- * themain
- * file*/
-int main(void){
-	int i;
-char c[]={"_putchar"};
-for(i=0;i<=7;i++)
+/**
+ * main - prints the string "_putchar" from a character array.
+ * Return: 0 on success.
+ */
+int main(void)
 {
-      _putchar(c[i]);
+	char c[] = "_putchar";
+	int i;
 
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+return (0);
 }
-return 0;
-}
+
 
 
