af181e06ca05636850f7ee8c7e1d1eb0522387b6
Biruk <leoseven59@gmail.com>
2022-06-07 15:26:43 +0300

    lol

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,2 +1,15 @@
 #include "main.h"
+/**
+ * void print_aplphabet(void) will print alphabet
+ * and it doesnt have return value
+ */
+void print_alphabet(void)
+{
+	char c;
+	for(c='a';c<='z';c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
 
